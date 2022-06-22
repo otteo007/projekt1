db19e6bc636f51eb819a12c575aacd5a9cc9c1a8
Oskar2k6 <oskar@trellert.de>
2022-06-22 14:34:50 +0200

    aadd 2 commit

--- script.js
+++ script.js
@@ -5,21 +5,13 @@
   }
 
   function lowerCaseName(string) {
-    return string.toLowerCase();           
+    return string.toLowerCase();
   }
 
   function heldItems(heldItems) {
-
-
-
-      for(let i = 0; i < heldItems.length; i++) {
-        return heldItems[i].item.name
-
-      }
-      
-      
-    
-
+    for(let i = 0; i < heldItems.length; i++) {
+      return heldItems[i].items.name;
+    }
   }
 
   function abilities(abilities) {
@@ -32,7 +24,7 @@
     const name = document.querySelector("#pokemonName").value;
     const pokemonName = lowerCaseName(name);
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    fetch(`https://pokeap.co/api/v2/pokemon/${pokemonName}`)
       .then((response) => response.json())
       .then((data) => {
         document.querySelector(".pokemonBox").innerHTML = `
@@ -45,7 +37,7 @@
         <div class="pokemonInfos">
           <h1><u>${capitalizeFirstLetter(data.name)}</u></h3>
           
-          <p>Das Pokemon hält folgendes item: ${heldItems(data.held_items)}.  ${capitalizeFirstLetter(data.name)} ist ein Pokémon mit einem Gewicht von ${data.weight}. Außerdem hat es eine Höhe von ${data.height}.
+          <p>Das Pokemon hält folgendes items: ${heldItems(data.held_items)}.  ${capitalizeFirstLetter(data.name)} ist ein Pokémon mit einem Gewicht von ${data.weight}. Außerdem hat es eine Höhe von ${data.height}.
           Das Pokémon hat die ID ${data.id}. Die Base xp die mann bekommt wenn man ${capitalizeFirstLetter(data.name)} besiegt sind ${data.base_experience}. Das Pokemon hat folgende abilities: ${abilities(data.abilities)}.</p>
 
           
