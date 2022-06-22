af3bb6e3b9b62935b2b7a0044bfda8eec9fa6f6e
Matteo <matteo.hauchwitz@gmail.com>
2022-06-22 11:45:41 +0200

    styles

--- script.js
+++ script.js
@@ -25,7 +25,7 @@
         <div class="pokemonInfos">
           <h1><u>${capitalizeFirstLetter(data.name)}</u></h3>
           <p>${capitalizeFirstLetter(data.name)} ist ein Pokémon mit einem Gewicht von ${data.weight}. Außerdem hat es eine Höhe von ${data.height}.
-          Das Pokémon hat eine ID von ${data.id}.</p>
+          Das Pokémon hat die ID ${data.id}.</p>
 
           
           
