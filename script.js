96ff34dd86029aca608dcb540d9a312194d2386f
moritz2k6 <moritztrellert@gmail.com>
2022-06-23 13:24:52 +0200

    change

--- script.js
+++ script.js
@@ -65,8 +65,8 @@ function getPokemon(e) {
       <div class="pokemonInfos">
         <h1><u>${capitalizeFirstLetter(data.name)}</u></h3>
         
-        <p>Das Pokemon hält ${heldItems(data.held_items)}.  ${capitalizeFirstLetter(data.name)} ist ein Pokémon mit einem Gewicht von ${data.weight}. Außerdem hat es eine Höhe von ${data.height}.
-        Das Pokémon hat die ID ${data.id}. Die Base xp die mann bekommt wenn man ${capitalizeFirstLetter(data.name)} besiegt sind ${data.base_experience}. Das Pokemon hat folgende abilities: ${abilities(data.abilities)}.</p>
+        <p>  ${capitalizeFirstLetter(data.name)} ist ein Pokémon mit einem Gewicht von ${data.weight}. Außerdem hat es eine Höhe von ${data.height}.Das Pokemon hält ${heldItems(data.held_items)}.
+        ${capitalizeFirstLetter(data.name)}hat die ID ${data.id}. Die Base xp die mann bekommt wenn man ${capitalizeFirstLetter(data.name)} besiegt sind ${data.base_experience}. Das Pokemon hat folgende abilities: ${abilities(data.abilities)}.</p>
 
         
         
